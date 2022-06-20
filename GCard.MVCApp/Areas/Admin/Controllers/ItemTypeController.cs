4211460ff43e0964cdeb2ac2fcb57b45912dda96
Rh Proj <r.proj@hotmail.com>
2022-06-20 21:17:46 +0300

    datatables

--- GCard.MVCApp/Areas/Admin/Controllers/ItemTypeController.cs
+++ GCard.MVCApp/Areas/Admin/Controllers/ItemTypeController.cs
@@ -8,17 +8,17 @@ namespace GCard.MVCApp.Areas.Admin.Controllers
     public class ItemTypeController : Controller
     {
         private readonly IRepositoryService _repoService;
-        private readonly IWebHostEnvironment _hostEnvironment;
-        public ItemTypeController(IRepositoryService repoService, IWebHostEnvironment hostEnvironment)
+        public ItemTypeController(IRepositoryService repoService)
         {
             _repoService = repoService;
-            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<ItemType> itemTypeList = _repoService.ItemTypeRepository.GetAll();
-            return View(itemTypeList);
+            //IEnumerable<ItemType> itemTypeList = _repoService.ItemTypeRepository.GetAll();
+            //return View(itemTypeList);
+
+            return View();
         }
 
         //GET
@@ -32,7 +32,7 @@ namespace GCard.MVCApp.Areas.Admin.Controllers
             //    CoverTypeList = _repoService.CoverTypeRepo.GetAll()
             //        .Select(i => new SelectListItem() { Text = i.Name, Value = i.Id.ToString() })
             //};
-            if (id == null || id == 0) //create
+            if (id == null || id == 0) 
             {
                 return View(itemType);//NotFound();
             }
@@ -43,12 +43,28 @@ namespace GCard.MVCApp.Areas.Admin.Controllers
             }
         }
 
-        //Post
+        //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(ItemType itemType)
         {
-            return NotFound();
+            if (ModelState.IsValid)
+            {
+                _repoService.ItemTypeRepository.Add(itemType);
+                _repoService.Save();
+                TempData["success"] = "Item type added succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(itemType);
         }
+
+        #region API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var itemTypeList = _repoService.ItemTypeRepository.GetAll();
+            return Json(new { data = itemTypeList });
+        }
+        #endregion
     }
 }
---
+++ GCard.MVCApp/wwwroot/js/itemtype.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/ItemType/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "displayOrder", "width": "15%" },
+            //{
+            //    "data": "id",
+            //    "render": function (data) {
+            //        return `
+            //        <div class="w-75 btn-group" role="group">
+            //            <a href="/Admin/Book/Upsert?id=${data}" 
+            //            class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+            //            <a onClick=Delete('/Admin/Book/Delete/${data}')
+            //            class="btn btn-danger mx-2"><i class="bi bi-x-circle"></i> Delete</a>
+            //        </div>
+            //        `
+            //    },
+            //    "width": "15%"
+            //}
+        ]
+    });
+}
